e5a4ef30d81f6f9380f4e524b569f6b9aaf3b103
ahmedsaid67 <96987324+ahmedsaid67@users.noreply.github.com>
2025-02-16 23:26:29 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['127.0.0.1','api.flexsoft.com.tr'], // Protokol ve sondaki eğik çizgi olmadan
+    domains: ['127.0.0.1','api.roseisguvenligiekipmanlari.com/'], // Protokol ve sondaki eğik çizgi olmadan
   },
 }
 
